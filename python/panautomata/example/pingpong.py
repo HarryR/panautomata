e7833d13587c6b618742c61df63706689052993e
Harry <gpg-dev-panautoma@midnight-labs.org>
2018-07-05 00:53:03 +0100

    Cleaned up debug prints in pingpong example

--- python/panautomata/example/pingpong.py
+++ python/panautomata/example/pingpong.py
@@ -39,44 +39,29 @@ def main():
     session_side_bob = (REAL_PROVER, 1, CONTRACT_B)
     session = (session_side_alice, session_side_bob, 1)
 
-    print("Guid", guid, hexlify(u256be(guid)))
+    print("Guid", hexlify(u256be(guid)))
 
     print("Start")
     tx = alice.Start(guid, session)
     start_receipt = tx.wait()
-    start_details = tx.details()
-    print("Start Receipt", start_receipt)
-    print("Start Receipt Details", start_details)
-    start_input = unhexlify(start_details['input'][2:])
-    print("Input hashed", hexlify(keccak_256(start_input).digest()))
     start_leaf, start_root, start_proof = proof_for_tx(rpc_a, tx)
-    start_leaf_hash = keccak_256(start_leaf).digest()
-    start_leaf_hash_int = bytes_to_int(start_leaf_hash)
-    start_leaf_hash_mint = bit_clear(start_leaf_hash_int, 0xFF)
-    print("Start Leaf", hexlify(start_leaf))
-    print("Start Leaf Hash", hexlify(start_leaf_hash),start_leaf_hash_int, start_leaf_hash_mint)
-    print("Start Root", start_root)
-    print("Start Proof", hexlify(start_proof))
 
     print("ReceiveStart")
     link_wait(link_b, start_proof)
     # TODO: wait until LithiumLink has been relayed the latest block
     tx = bob.ReceiveStart(guid, session, start_proof)
     receive_start_receipt = tx.wait()
-    print("Receieve Start Receipt", receive_start_receipt)
     ping_proof = proof_for_event(rpc_b, tx, 0)
 
     for _ in range(0, 5):
         print("ReceivePing")
-        link_wait(link_a, ping_proof)
-        # TODO: wait until LithiumLink has been relayed the latest block
+        link_wait(link_a, ping_proof)        
         tx = alice.ReceivePing(guid, ping_proof)
         pong_proof = proof_for_event(rpc_a, tx, 0)
         receive_ping_receipt = tx.wait()
 
         print("ReceivePong")
         link_wait(link_b, pong_proof)
-        # TODO: wait until LithiumLink has been relayed the latest block
         tx = bob.ReceivePong(guid, pong_proof)
         ping_proof = proof_for_event(rpc_b, tx, 0)
         receive_pong_receipt = tx.wait()
